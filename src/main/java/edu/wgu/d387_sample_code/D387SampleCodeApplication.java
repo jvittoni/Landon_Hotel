aa71983c90d8f2598cd0a7adf2f8b70350ee01a2
Javae Vittone <jvitton@wgu.edu>
2024-07-30 08:35:11 -0700

    File Name: D387SampleCodeApplication.java / Line: 18 - 26 / Edit: Created threads for welcome messages

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,28 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.translation.WelcomeMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import edu.wgu.d387_sample_code.translation.WelcomeMessageController;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-	}
 
+		// Thread For English Welcome Message
+		WelcomeMessage welcomeMessageEnglish = new WelcomeMessage(Locale.US);
+		Thread englishWelcomeThread = new Thread(welcomeMessageEnglish);
+		englishWelcomeThread.start();
+
+		// Thread For French Welcome Message
+		WelcomeMessage welcomeMessageFrench = new WelcomeMessage(Locale.CANADA_FRENCH);
+		Thread frenchWelcomeThread = new Thread(welcomeMessageFrench);
+		frenchWelcomeThread.start();
+	}
 }
