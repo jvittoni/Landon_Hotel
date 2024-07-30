60591d93b647c71c5821816e075e3b39b66d5e60
Javae Vittone <jvitton@wgu.edu>
2024-07-29 21:55:13 -0700

    File Name: WelcomeMessage.java / Line: 1 - 21 / Edit: Created file and created class for welcome messages

---
+++ src/main/java/edu/wgu/d387_sample_code/translation/WelcomeMessage.java
@@ -0,0 +1,21 @@
+package edu.wgu.d387_sample_code.translation;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class WelcomeMessage implements Runnable {
+    Locale locale;
+
+    //Constructor
+    public WelcomeMessage(Locale locale) {
+        this.locale = locale;
+    }
+
+    public String getWelcomeMessage() {
+        ResourceBundle bundle = ResourceBundle.getBundle("translation", locale);
+        return bundle.getString("welcome");
+    }
+
+    @Override
+    public void run() {}
+}
